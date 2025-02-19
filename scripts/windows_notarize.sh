8c836d03c53d183863b037abf553a57265ad9814
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-19 16:58:37 +0100

    windows

--- scripts/windows_notarize.sh
+++ scripts/windows_notarize.sh
@@ -2,15 +2,15 @@
 
 set -Eeou pipefail
 
-echo "TEMP signing ${artifact}"
+EXE_FILE="./dist/windows_windows_amd64_v1/binary.exe"
 
-if [[ -f "${artifact:?}" ]]; then
-	echo "signing ${artifact}"
+if [[ -f "$EXE_FILE" ]]; then
+	echo "signing Windows binary: ${EXE_FILE}"
 
   docker run \
     -e GRS_CONFIG_USER1_USERNAME="${ARTIFACTORY_SIGN_USER}" \
     -e GRS_CONFIG_USER1_PASSWORD="${ARTIFACTORY_SIGN_PASSWORD}" \
 		--rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" \
     "${ARTIFACTORY_REGISTRY}/release-tools-container-registry-local/garasign-jsign" \
-		/bin/bash -c "jsign --tsaurl http://timestamp.digicert.com -a ${AUTHENTICODE_KEY_NAME} \"${artifact}\""
+		/bin/bash -c "jsign --tsaurl http://timestamp.digicert.com -a ${AUTHENTICODE_KEY_NAME} \"${EXE_FILE}\""
 fi
