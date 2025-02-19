d98e8fdac8600c2885353ad9833e6535265933f4
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-19 16:44:40 +0100

    remove mac

--- scripts/windows_notarize.sh
+++ scripts/windows_notarize.sh
@@ -2,18 +2,13 @@
 
 set -Eeou pipefail
 
-EXE_FILE="./dist/windows_windows_amd64_v1/binary.exe"
-
-echo "EXE_FILE: $EXE_FILE"
-echo "artifact: $artifact"
-
-if [[ -f "$EXE_FILE" ]]; then
-	echo "signing $EXE_FILE"
+if [[ -f "${artifact:?}" ]]; then
+	echo "signing ${artifact}"
 
   docker run \
     -e GRS_CONFIG_USER1_USERNAME="${ARTIFACTORY_SIGN_USER}" \
     -e GRS_CONFIG_USER1_PASSWORD="${ARTIFACTORY_SIGN_PASSWORD}" \
 		--rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" \
     "${ARTIFACTORY_REGISTRY}/release-tools-container-registry-local/garasign-jsign" \
-		/bin/bash -c "jsign --tsaurl http://timestamp.digicert.com -a ${AUTHENTICODE_KEY_NAME} \"$EXE_FILE\""
+		/bin/bash -c "jsign --tsaurl http://timestamp.digicert.com -a ${AUTHENTICODE_KEY_NAME} \"${artifact}\""
 fi
