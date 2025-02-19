385352ce41643413f77b263de426f936256b4c5b
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-19 09:29:24 +0100

    env vars

--- scripts/notarize.sh
+++ scripts/notarize.sh
@@ -2,12 +2,14 @@
 
 set -Eeou pipefail
 
+echo "GRS_CONFIG_USER1_USERNAME=${ARTIFACTORY_SIGN_USER}" >> "signing-envfile"
+echo "GRS_CONFIG_USER1_PASSWORD=${ARTIFACTORY_SIGN_PASSWORD}" >> "signing-envfile"
+
 if [[ -f "${artifact:?}" ]]; then
   echo "notarizing package ${artifact}"
 
   docker run \
-    -e GRS_CONFIG_USER1_USERNAME="${ARTIFACTORY_SIGN_USER}" \
-    -e GRS_CONFIG_USER1_PASSWORD="${ARTIFACTORY_SIGN_PASSWORD}" \
+    --env-file=signing-envfile \
     --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" \
     "${ARTIFACTORY_REGISTRY}/release-tools-container-registry-local/garasign-gpg" \
     /bin/bash -c "gpgloader && gpg --yes -v --armor -o ${artifact}.sig --detach-sign ${artifact}"
