87cfa01ace80a106ad30841a8b349b3ce1c6ad04
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-19 13:20:40 +0100

    fix mac paths

--- scripts/mac_notarize.sh
+++ scripts/mac_notarize.sh
@@ -2,14 +2,14 @@
 
 set -Eeou pipefail
 
-if [[ -f "./dist/macos_darwin_amd64_v1/binary" && -f "./dist/macos_darwin_arm64/binary" && ! -f "./dist/binary_signed.zip" ]]; then
+if [[ -f "./dist/macos_darwin_amd64_v1/binary" && -f "./dist/macos_darwin_arm64_v8.0/binary" && ! -f "./dist/mac_bin_signed.zip" ]]; then
 	echo "notarizing macOs binaries"
 
 	curl "${NOTARY_SERVICE_URL}" --output macos-notary.zip
 	unzip -u macos-notary.zip
 	chmod 755 ./linux_amd64/macnotary
 
-	zip -r ./dist/mac_bin.zip ./dist/macos_darwin_amd64_v1/binary ./dist/macos_darwin_arm64/binary # The Notarization Service takes an archive as input
+	zip -r ./dist/mac_bin.zip ./dist/macos_darwin_amd64_v1/binary ./dist/macos_darwin_arm64_v8.0/binary # The Notarization Service takes an archive as input
 	./linux_amd64/macnotary \
 		-f ./dist/mac_bin.zip \
 		-m notarizeAndSign -u https://dev.macos-notary.build.10gen.cc/api \
@@ -18,5 +18,5 @@ if [[ -f "./dist/macos_darwin_amd64_v1/binary" && -f "./dist/macos_darwin_arm64/
 
 	echo "replacing original files"
 	unzip -oj ./dist/mac_bin_signed.zip dist/macos_darwin_amd64_v1/binary -d ./dist/macos_darwin_amd64_v1/
-	unzip -oj ./dist/mac_bin_signed.zip dist/macos_darwin_arm64/binary -d ./dist/macos_darwin_arm64/
+	unzip -oj ./dist/mac_bin_signed.zip dist/macos_darwin_arm64_v8.0/binary -d ./dist/macos_darwin_arm64_v8.0/
 fi
