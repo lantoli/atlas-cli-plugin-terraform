c2fa41bef2ca535bd53e8b1a4bb4e431974e48dc
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-03-15 12:59:24 +0100

    try openai

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.1
 require (
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/hashicorp/hcl/v2 v2.23.0
+	github.com/openai/openai-go v0.1.0-alpha.62
 	github.com/sebdah/goldie/v2 v2.5.5
 	github.com/spf13/afero v1.12.0
 	github.com/spf13/cobra v1.9.1
@@ -22,6 +23,10 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
--- internal/cli/clu2adv/opts.go
+++ internal/cli/clu2adv/opts.go
@@ -1,12 +1,15 @@
 package clu2adv
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/mongodb-labs/atlas-cli-plugin-terraform/internal/convert"
 	"github.com/mongodb-labs/atlas-cli-plugin-terraform/internal/file"
+	"github.com/openai/openai-go"
 	"github.com/spf13/afero"
 )
 
@@ -53,6 +56,34 @@ func (o *opts) generateFile(allowParseErrors bool) error {
 			return err
 		}
 	}
+
+	promptPrefix := "# prompt: "
+	inStr := string(inConfig)
+	outStr := string(outConfig)
+	if strings.HasPrefix(inStr, promptPrefix) {
+		prompt := strings.SplitN(inStr, "\n", 2)[0][len(promptPrefix):]
+		promptOut := fmt.Sprintf(`
+			Input File: """%s"""
+			Output File: """%s"""
+			We want to transform the Input File that is an HCL Terraform file with mongodbatlas_cluster resources into an Output File that is also an HCL Terraform file with mongodbatlas_advanced_cluster resources.
+			We will ignore any resources that are not mongodbatlas_cluster or mongodbatlas_advanced_cluster and keep as it is.
+			Your response must be a valid HCL Terraform file, please make sure to keep the syntax correct and write in the content of the Output File using comments with #.
+			This is what I want to do in the Output File: """%s""".
+		`, inStr, outStr, prompt)
+		client := openai.NewClient()
+		chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+			Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage(promptOut),
+			}),
+			Model: openai.F(openai.ChatModelGPT4o),
+		})
+		if err == nil {
+			outConfig = []byte(chatCompletion.Choices[0].Message.Content)
+		} else {
+			fmt.Println("Failed to get completion from OpenAI: ", err)
+		}
+	}
+
 	if err := afero.WriteFile(o.fs, o.output, outConfig, 0o600); err != nil {
 		return fmt.Errorf("failed to write file %s: %w", o.output, err)
 	}
